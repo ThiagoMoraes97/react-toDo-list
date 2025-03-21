9a2e1539331dba06bc5aa89155ee6eb3b6981391
Thiago Moraes <thiagomottamoraes@gmail.com>
2025-03-21 16:08:34 -0300

    Header completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import './global.css'
+import styles from './App.module.css'
+import { MdAddCircleOutline } from "react-icons/md";
+
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.page}>
+      <header className={styles.header}> 
+        <img src="/public/rocketLogo.svg" alt="Logo" />
+      </header>
+
+      <main className={styles.main}>
+        <form>
+          <input type="text" placeholder='Adicione uma nova tarefa'/>
+          <button type="submit">Criar <MdAddCircleOutline/></button>
+        </form>
+
+        <section className={styles.tasks}>
+          <header className={styles.tasksHeader}>
+            <div className={styles.tasksHeaderInfo}>
+              <div className={styles.tasksCreated}></div>
+              <div className={styles.tasksCompleted}></div>
+            </div>
+          </header>
+          <div className={styles.tasksList}>
+            
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
